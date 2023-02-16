51783900d7117064c806c6a906bb6f00800bb5ba
gérald <gerald@hairun-technology.com>
2023-02-16 13:59:38 +0300

    added more realistic fake data with javaFaker lib

--- src/main/java/com/example/employee/controller/SeedController.java
+++ src/main/java/com/example/employee/controller/SeedController.java
@@ -4,11 +4,14 @@ import com.example.employee.model.Company;
 import com.example.employee.model.Employee;
 import com.example.employee.service.CompanyService;
 import com.example.employee.service.EmployeeService;
+import com.github.javafaker.Faker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 @RequestMapping("/api")
@@ -21,6 +24,7 @@ public class SeedController {
 
     @GetMapping(value = "/seed/{nb}")
     public void launchSeeder(@PathVariable(value = "nb") Long nb){
+        Faker faker = new Faker(new Locale("fr"));
         // clean every table
         employeeService.cleanEmployees();
         companyService.cleanCompanies();
@@ -28,15 +32,16 @@ public class SeedController {
         // create {nb * 20} employees
         for (int i = 0; i < nb; i++) {
             Company cmp = new Company();
-            cmp.setName("Company_"+i);
+            String companyName = faker.company().name();
+            cmp.setName(companyName);
             cmp = companyService.createCompany(cmp);
             for (int j = 0; j < 20; j++) {
                 Employee emp = new Employee();
-                String firstname = "First" + j + "-" + i;
-                String lastname = "Last" + j + "-" + i;
+                String firstname = faker.name().firstName();
+                String lastname = faker.name().lastName();
                 emp.setFirstName(firstname);
                 emp.setLastName(lastname);
-                emp.setEmailId(firstname + "." + lastname + "_@"+"Company_" + i + ".com");
+                emp.setEmailId(firstname + "." + lastname + "@"+ companyName.trim().replace(' ', '-')+ ".com");
                 emp.setCompany(cmp);
                 employeeService.createEmployee(emp);
             }
